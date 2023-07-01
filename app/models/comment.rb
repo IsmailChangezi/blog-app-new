542bdffddcc659dc53cee4bbe5757241e531b9ba
Ismail Ali <ismailchangezi801@gmail.com>
2023-07-02 02:27:50 +0500

    Fix Linter Errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    after_save :update_comment_counter
-    private
-    
-    def update_comment_counter
-        post.increment!(:comments_counter)
-    end
+  belongs_to :post
+  after_save :update_comment_counter
+
+  private
+
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    after_save :post_counter_update
+  after_save :post_counter_update
 
-    validates :title, presence: true, length: { maximum: 250 }
-    validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-    validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-    def most_recent_comments
-      comments.order(created_at: :desc).limit(5)
-    end
-  
-    private
-  
-    def post_counter_update
-      author.increment!(:posts_counter)
-    end
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def post_counter_update
+    author.increment!(:posts_counter)
+  end
 end
