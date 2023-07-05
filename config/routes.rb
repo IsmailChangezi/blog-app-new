1852db18e09883bf5d203bed122afd2054ca1b1f
Ismail Ali <ismailchangezi801@gmail.com>
2023-07-05 13:09:07 +0500

    Controller Specs: Implement Required Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] 
-    resources :posts, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root :to => 'users#index'
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
+  # root "users#index"
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /users/:user_id/posts' do
-    it 'works! (now write some real specs)' do
-      get '/users/1/posts'
-      expect(response).to have_http_status(200)
-      expect(response).to render_template('posts/index')
-      expect(response.body).to include('Posts from user 1')
-    end
-  end
-
   describe 'GET /users/:user_id/posts/:id' do
     it 'works! (now write some real specs)' do
       get '/users/1/posts/1'
