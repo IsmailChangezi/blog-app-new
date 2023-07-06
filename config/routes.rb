481efdad5b7ff207e077c06ad6c8294c9dde5594
Ismail Ali <ismailchangezi801@gmail.com>
2023-07-06 17:31:24 +0500

    Implement Interactive View

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root :to => 'users#index'
+  # root "articles#index"
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show]
   end
-  # root "users#index"
 end
