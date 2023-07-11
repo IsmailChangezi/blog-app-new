28955038250c0642c33c52f72389d6b9b6400133
Ismail Ali <ismailchangezi801@gmail.com>
2023-07-11 15:48:30 +0500

    Fix n+1 issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,19 +2,20 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  has_many :most_recent_comments, -> { order(created_at: :desc).limit(5) }, class_name: 'Comment'
+
   after_save :post_counter_update
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  def most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  private
-
   def post_counter_update
     author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
