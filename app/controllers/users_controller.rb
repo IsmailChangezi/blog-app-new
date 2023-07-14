aaab30c680fccd92bd1e30ab8d46ab0d44d3712d
matee-safi <matiullahsafibbc@gmail.com>
2023-07-14 15:03:54 +0430

    remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,4 @@ class UsersController < ApplicationController
     @posts = @user.most_recent_posts
     @posts = Post.where(author_id: @user.id)
   end
-
-  # def new
-  #  @user = User.new
-  # end
-
-  # def create
-  #  @user = User.create(params[:user])
-  #  if @user.save
-  #    redirect_to user_path(@user.id)
-  #  else
-  #    render :new
-  #  end
-  # end
 end
